c51af5b39c04e2372c1f15e292c843ef5dc20cf7
chinhcomhut <chinhcomhut@gmail.com>
2019-12-16 16:39:47 +0700

    lan 1

---
+++ src/main/java/com/security/demospringsecurity/service/Impl/SongServiceImpl.java
@@ -0,0 +1,61 @@
+package com.security.demospringsecurity.service.Impl;
+
+import com.security.demospringsecurity.model.Song;
+import com.security.demospringsecurity.repository.SongRepository;
+import com.security.demospringsecurity.service.SongService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+@Service
+public class SongServiceImpl implements SongService {
+
+    @Autowired
+    private SongRepository songRepository;
+
+    @Override
+    public Optional<Song> findByNameSongContaining(String song) {
+        return songRepository.findByNameSongContaining(song);
+    }
+
+    @Override
+    public List<Song> findAllByUserId(Long userId) {
+        return songRepository.findAllByUserId(userId);
+    }
+
+    @Override
+    public List<Song> findAllByOrderByLikeSong() {
+        return songRepository.findAllByOrderByLikeSongDesc();
+    }
+
+    @Override
+    public List<Song> findAllByOrderByListenSong() {
+        return songRepository.findAllByOrderByListenSongDesc();
+    }
+
+    @Override
+    public List<Song> findAll() {
+        return songRepository.findAll();
+    }
+
+    @Override
+    public Optional<Song> findById(long id) {
+        return songRepository.findById(id);
+    }
+
+    @Override
+    public Song save(Song song) {
+        return songRepository.save(song);
+    }
+
+    @Override
+    public void delete(long id) {
+        songRepository.deleteById(id);
+    }
+
+    @Override
+    public Song findByIdSong(Long id) {
+        return songRepository.findById(id).get();
+    }
+}
---
+++ src/main/java/com/security/demospringsecurity/service/SongService.java
@@ -0,0 +1,29 @@
+package com.security.demospringsecurity.service;
+
+import com.security.demospringsecurity.model.Song;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public interface SongService {
+
+    List<Song> findAllByOrderByLikeSong();
+
+    List<Song> findAllByOrderByListenSong();
+
+    List<Song> findAllByUserId (Long userId);
+
+    List<Song> findAll();
+
+    Optional<Song> findById(long id);
+
+    Song findByIdSong(Long id);
+
+    Song save(Song song);
+
+    void delete(long id);
+
+    Optional<Song> findByNameSongContaining(String song);
+}
